e42743c3e368abb566eca67b93b146d8e5f5f15a
Aytenew <agashaye04@gmail.com>
2022-07-01 00:42:27 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-/**
- * main - entry point
- *
- * Return: 0 after printing the fuction
- */
-int main (void)
-{puts("\"programming is like building a multiligual puzzle");
-	return(0);
+int main(void)
+{
+	// string initialisation
+	char mystr [] = "Programming is like building a multilingual puzzle";
+	puts(mystr); writing the string stout
+
+	return 0;
 }
