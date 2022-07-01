4c268eff2d9010504b72354e1a96c6ccac7d50b8
Aytenew <agashaye04@gmail.com>
2022-07-01 01:31:24 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,8 @@
  *
  * return: 0 after printing the function 
  */
-int main(void)
+int main(void)// this specifies there are no arguments taken by main
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts ("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
