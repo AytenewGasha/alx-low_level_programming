49274d89961b4102235a9fa52ccc4f5f0472c5fd
Aytenew <agashaye04@gmail.com>
2022-07-01 01:02:28 -0700

    delete task4

--- 0x00-hello_world/4-puts.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(void)
-{
-	// string initialisation
-	char mystr [] = "Programming is like building a multilingual puzzle";
-	puts(mystr); writing the string stout
-
-	return 0;
-}
